a2486439e555e42a7827528481f4bd69a6341e87
clpy9793 <wqs1639@gmail.com>
2017-06-12 18:25:56 +0800

    .

---
+++ src/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-06-12 10:53:38
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+
+
+ACCOUNT_HOST = ''
+ACCOUNT_PORT = ''
+
+
+GATEWAY_HOST = ''
+GATEWAY_PORT = ''
+
+
+PACKAGE_KEY = ''
